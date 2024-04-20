227206893834cfecbec9467f2797e03993e13f7a
vinend <vinendsocials@gmail.com>
2024-04-20 15:14:37 +0700

    navbar improvement, nambah interaktifitas dengan gambar

--- my-project/src/component/homepage/Hero.jsx
+++ my-project/src/component/homepage/Hero.jsx
@@ -6,12 +6,12 @@ const Hero = () => {
     <div className='py-[50px] px-[50px] flex flex-row gap-10 bg-yellow-300'>
         <div className="flex flex-col justify-center md:justify-start md:items-start items-center md:w-[50%] gap-[10px]">
             <h1 className='text-[50px] font-bold md:text-start'>OUR HERO</h1>
-            <img src={fighter} className='h-[200px] md:hidden rounded'/>
+            <img src={fighter} className='h-[200px] md:hidden rounded transition-transform duration-300 ease-in-out hover:scale-110'/>
             <p className='text-center md:text-start text-[20px]'>
                 Vishwakarma is a team from the University of Indonesia specializing in VTOL (Vertical Take-Off and Landing) technology. They are dedicated to advancing and innovating in the field of aerial vehicles. The team consists of passionate and motivated individuals who strive to push the boundaries of aviation engineering. We actively participate in various competitions and projects, showcasing their expertise and technical skills. Our commitment to excellence and continuous learning makes them a valuable asset in the field of aviation technology.
             </p>
         </div>
-        <img src={fighter} className='h-[350px] md:flex hidden mx-auto my-auto rounded'/>
+        <img src={fighter} className='h-[350px] md:flex hidden mx-auto my-auto rounded transition-transform duration-300 ease-in-out hover:scale-110'/>
     </div>
   )
 }
--- my-project/src/component/homepage/Krti.jsx
+++ my-project/src/component/homepage/Krti.jsx
@@ -5,10 +5,10 @@ import CounterContainer from './CounterContainer'
 const Krti = () => {
   return (
     <div className='py-[50px] px-[50px] flex flex-row gap-10 bg-yellow-600'>
-        <img src={logoKrti} className='h-[300px] md:flex hidden mx-auto my-auto'/>
+        <img src={logoKrti} className='h-[300px] md:flex hidden mx-auto my-auto transition-transform duration-300 ease-in-out hover:scale-110'/>
         <div className="flex flex-col justify-center md:justify-start md:items-start items-center gap-[20px] md:w-[50%]">
             <h1 className='text-[50px] font-bold md:text-start'>KRTI</h1>
-            <img src={logoKrti} className='h-[200px] md:hidden'/>
+            <img src={logoKrti} className='h-[200px] md:hidden transition-transform duration-300 ease-in-out hover:scale-110'/>
             <p className='text-center md:text-start text-[20px]'>
             Vishwakarma's goal this year, is to tackle the challenges of winning KRTI (Kontes Robot Terbang Indonesia) 2024 and also future competitions national or international. Through our hardworking members and dedication to developing unmanned aerial vehicle technology, we strive to take champion title this year. With the gist of the competition coming up, we are working to achieve the best outcome possible and innovate with the technology we have.
             </p>
