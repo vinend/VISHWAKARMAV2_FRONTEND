d3d6841bf1823eb411418a573ed958ef25710408
vinend <vinendsocials@gmail.com>
2024-04-20 15:08:42 +0700

    improve navbar

--- my-project/src/component/homepage/CounterAtom.jsx
+++ my-project/src/component/homepage/CounterAtom.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
 import Atom from "../../assets/Atom.png";
+import '../../index.css';
 
 const CounterAtom = ({ time, timeleft, isHidden = false }) => {
   return (
-    <div className={`${isHidden && "hidden md:flex"} relative`}>
-      <img src={Atom} height={100} width={100} />
+    <div className={`${isHidden ? "hidden md:flex" : ""} relative`}>
+      <img src={Atom} height={100} width={100} alt="Atom Logo" />
       <div className="absolute top-1/4 right-[36%]">
-        <p className="font-bold text-center text-base lg:text-2xl">{timeleft}</p>
-        <p className="text-center text-[12px] sm:text-sm">{time}</p>
+        <p className="font-bold text-center text-base lg:text-2xl" style={{ fontFamily: 'Digital-7' }}>{timeleft}</p>
+        <p className="text-center text-[12px] sm:text-sm" style={{ fontFamily: 'Digital-7' }}>{time}</p>
       </div>
     </div>
   );
 };
 
 export default CounterAtom;
+
--- my-project/src/component/homepage/Navbar.jsx
+++ my-project/src/component/homepage/Navbar.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import auav from '../../assets/image.png'
-import vishmakara from '../../assets/vtol.png'
-import hiddenMenu from '../../assets/1366x768-269dd16fa1f5ff51accd09e7e1602267.png'
-import location from '../../assets/location.svg'
-import linkedin from '../../assets/linkedin.png'
-import instagram from '../../assets/instagram.png'
-import facebook from '../../assets/facebook.png'
-import twitter from '../../assets/twitter.png'
+import React from 'react';
+import auav from '../../assets/image.png';
+import vishmakara from '../../assets/vtol.png';
+import hiddenMenu from '../../assets/1366x768-269dd16fa1f5ff51accd09e7e1602267.png';
 
 const Navbar = () => {
     return (
-        <nav className='px-[10px] py-[10px] flex flex-row bg-zinc-800 z-30 sticky top-0 gap-[20px]'>
-            <img src={auav} className='h-[50px]'/>
-            <img src={vishmakara} className='h-[50px] md:hidden'/>
-            <div className="w-full flex md:justify-start md:flex-row md:gap-[20px] justify-end">
-                <img src={hiddenMenu} className='h-[50px] w-[50px] md:hidden cursor-pointer'/>
-                <a className='text-white py-[10px] hidden cursor-pointer md:block'>Home</a>
-                <a className='text-white py-[10px] hidden cursor-pointer md:block'>Profile</a>
-                <a className='text-white py-[10px] hidden cursor-pointer md:block'>Achievement</a>
-                <a className='text-white py-[10px] hidden cursor-pointer md:block'>Contact</a>
-            </div>
-            <img src={vishmakara} className='h-[50px] hidden md:block'/>
-        </nav>
-    )
+        <>
+            <nav className='flex items-center justify-between px-2.5 py-2.5 bg-zinc-800 sticky top-0 z-30 transition-all duration-300 ease-in-out hover:border-yellow-500 border border-transparent'>
+                <img src={auav} className='h-12'/>
+                <div className="hidden md:flex gap-[75px]">
+                    <a className='text-white py-2.5 glow-on-hover'>Home</a>
+                    <a className='text-white py-2.5 glow-on-hover'>Profile</a>
+                    <a className='text-white py-2.5 glow-on-hover'>Achievement</a>
+                    <a className='text-white py-2.5 glow-on-hover'>Contact</a>
+                </div>
+                <img src={vishmakara} className='h-12'/>
+                <img src={hiddenMenu} className='h-12 w-12 md:hidden cursor-pointer'/>
+            </nav>
+            <style>
+                {`
+                .glow-on-hover:hover {
+                    text-shadow:
+                        0 0 10px #fff, /* white glow */
+                        0 0 20px #f1c40f; /* yellow glow */
+                }
+                `}
+            </style>
+        </>
+    );
 }
 
+export default Navbar;
 
-    export default Navbar
