cb5ef305e0fa17bb4344a46c215fcb2cf6171561
vinend <vinendsocials@gmail.com>
2024-05-01 20:10:05 +0700

    design homepage update

--- my-project/src/component/homepage/HomePage.jsx
+++ my-project/src/component/homepage/HomePage.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Vismakara from './Vismakara'
-import Krti from './Krti'
-import Footer from './Footer'
-import Hero from './Hero'
+import React from 'react';
+import Navbar from './Navbar';
+import Vismakara from './Vismakara';
+import Krti from './Krti';
+import Footer from './Footer';
+import Hero from './Hero';
 
 const HomePage = () => {
   return (
-    <>
+    <div className="flex-grow bg-gray-700" style={{ paddingBottom: '5px' }}>
         <Navbar/>
-        <Vismakara/>
-        <Krti/>
-        <Hero/>
-        <Footer/>
-    </>
+        <div className="my-4 border-4 border-yellow-500 p-3">
+            <Vismakara/>
+        </div>
+        <div className="my-4 border-4 border-yellow-500 p-3">
+            <Krti/>
+        </div>
+        <div className="my-4 border-4 border-yellow-500 p-3">
+            <Hero/>
+        </div>
+            <Footer/>
+    </div> 
   )
 }
 
-export default HomePage
+export default HomePage;
