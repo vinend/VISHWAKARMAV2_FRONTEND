710597c6bdccb5aa12761e2d6abf0657011a3ea2
vinend <vinendsocials@gmail.com>
2024-06-01 15:24:59 +0700

    fix backround, tambahin caption di awal

--- my-project/src/component/LandingPage/LandingPage.jsx
+++ my-project/src/component/LandingPage/LandingPage.jsx
@@ -1,37 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import bgMono from '../../assets/JOR093763.jpg'
-import bgMono2 from '../../assets/JOR093764.jpg'
-import Drone from '../../assets/Drone.png'
+import bgMono from '../../assets/JOR093763.jpg';
+import bgMono2 from '../../assets/JOR093764.jpg';
+import Drone from '../../assets/drone2.png';
 
 const LandingPage = () => {
-  const [backgroundImage,setBackgroundImage] = useState(window.innerWidth >= 768 ? bgMono : bgMono2);
+  const [backgroundImage, setBackgroundImage] = useState(window.innerWidth >= 768 ? bgMono : bgMono2);
 
   useEffect(() => {
     const handleResize = () => {
-        setBackgroundImage(window.innerWidth >= 768 ? bgMono : bgMono2);
+      setBackgroundImage(window.innerWidth >= 768 ? bgMono : bgMono2);
     };
     window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
-    <div className='h-screen w-screen md:px-[40px] bg-slate-500 text-yellow-500 flex-row bg-no-repeat bg-cover flex' style={{backgroundImage: `url(${backgroundImage})`}}>
-        <div className="flex md:items-start items-center h-screen justify-center flex-col w-full">
-            <p className='text-[100px] font-poppins font-bold md:block hidden'>VISHMAKARMA</p>
-            <p className='text-[90px] font-poppins font-bold md:hidden block'>VISHMA</p>
-            <p className='text-[90px] font-poppins font-bold md:hidden block'>KARMA</p>
-            <p className='text-[20px] font-poppins font-bold'>our vision</p>
-
-            <div className=' flex justify-center items-center w-full'> 
-              <a href="/home" className='my-[70px] px-[20px] py-[5px] outline outline-2 rounded-2xl cursor-pointer font-bold bg-yellow-500 text-white hover:bg-black hover:text-yellow-500'>Read About Us {'>>>'}</a>            
-            </div>
+    <div className='h-screen w-screen md:px-[40px] bg-slate-500 text-yellow-500 flex-row bg-no-repeat bg-cover flex' style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="flex md:items-start items-center h-screen justify-center flex-col w-full">
+        <p className='text-[100px] font-poppins font-bold md:block hidden'>VISHMAKARMA</p>
+        <p className='text-[90px] font-poppins font-bold md:hidden block'>VISHMA</p>
+        <p className='text-[90px] font-poppins font-bold md:hidden block'>KARMA</p>
+        <div className='bg-yellow-500 text-white p-4 rounded-lg'>
+          <p className='text-[20px] font-poppins font-bold'>
+            The Vishwakarma Aerial Dexterity Research Group (ADRG) founded in 2013 has a strong record in various competitions, including the Brawijaya Copter Competition, the Parahyangan Robotic Competition, and the Annual Indonesian Flying Robot Contest, organized by the Indonesian Ministry of Research and Higher Education
+          </p>
         </div>
-        <div className=" items-center justify-center w-[100%] md:flex hidden">
-          <div className="w-[400px] h-[400px] z-5 bg-yellow-500 rounded-xl flex justify-center items-center">
-            <img src={Drone} className='z-10'/>
-          </div>
+        <div className='flex justify-center items-center w-full'>
+          <a href="/home" className='my-[70px] px-[20px] py-[5px] outline outline-2 rounded-2xl cursor-pointer font-bold bg-yellow-500 text-white hover:bg-black hover:text-yellow-500'>
+            Read About Us {'>>>'}
+          </a>
         </div>
+      </div>
+      <div className="items-center justify-center w-[100%] md:flex hidden">
+        <div className="w-[400px] h-[400px] z-5 bg-yellow-500 rounded-xl flex justify-center items-center">
+          <img src={Drone} className='z-10' />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
--- my-project/src/component/achievement/Achieve.jsx
+++ my-project/src/component/achievement/Achieve.jsx
@@ -2,21 +2,27 @@ import React, { Fragment } from 'react';
 import eventsData from './eventComponents'; // Assuming this import brings the events data
 import Navbar from '../homepage/Navbar';
 import Footer from '../homepage/Footer';
-import bgContactSmall from '../../assets/17148025170861.jpg'
-
-
+import bgContactSmall from '../../assets/kebersamaanVTOL.jpg';
 
 const Achieve = ({ events }) => {
   return (
-    <div className={`flex flex-col bg-cover bg-no-repeat min-h-screen`} style={{ backgroundImage: `url(${bgContactSmall})`}}> {/* Ensure full screen height and flex column layout */}
+    <div className="relative flex flex-col min-h-screen">
+      <div
+        className="absolute inset-0 bg-cover bg-no-repeat filter grayscale bg-center"
+        style={{
+          backgroundImage: `url(${bgContactSmall})`,
+          zIndex: -1,
+        }}
+      ></div>
+
       <Navbar />
 
-      <div className='flex-grow flex flex-col gap-y-3 my-4 overflow-auto'> {/* Container for scrolling content */}
+      <div className="flex-grow flex flex-col gap-y-3 my-4 overflow-auto">
         <Circle /> {/* First circle appears here */}
 
         {events.map((event, key) => (
           <Fragment key={key}>
-            <div className='grid grid-cols-[1fr_auto_1fr] gap-x-2 items-center mx-auto'>
+            <div className="grid grid-cols-[1fr_auto_1fr] gap-x-2 items-center mx-auto">
               {event.direction === 'left' ? (
                 <EventCard {...event} />
               ) : (
@@ -31,7 +37,7 @@ const Achieve = ({ events }) => {
                 <div></div>
               )}
             </div>
-            {key < (events.length - 1) && <Circle />}
+            {key < events.length - 1 && <Circle />}
           </Fragment>
         ))}
 
@@ -41,23 +47,22 @@ const Achieve = ({ events }) => {
       <Footer />
     </div>
   );
-}
+};
 
 const Circle = () => (
-  <div className='rounded-full w-4 h-4 bg-yellow-500 mx-auto'></div>
+  <div className="rounded-full w-4 h-4 bg-yellow-500 mx-auto"></div>
 );
 
 const Pillar = () => (
-  <div className='rounded-t-full rounded-b-full w-2 h-full bg-yellow-500 mx-auto'></div>
+  <div className="rounded-t-full rounded-b-full w-2 h-full bg-yellow-500 mx-auto"></div>
 );
 
 const EventCard = ({ heading, subheading, image }) => (
-  <div className='flex flex-col bg-white gap-y-2 border shadow-md rounded-xl p-4'>
-    <img src={image} alt={heading} className='w-full h-32 object-cover rounded-md' /> {/* Display image */}
-    <div className='text-grey-800 font-bold text-lg border-b'>{heading}</div>
-    <div className='text-sm text-gray-700'>{subheading}</div>
+  <div className="flex flex-col bg-white gap-y-2 border shadow-md rounded-xl p-4">
+    <img src={image} alt={heading} className="w-full h-32 object-cover rounded-md" /> {/* Display image */}
+    <div className="text-grey-800 font-bold text-lg border-b">{heading}</div>
+    <div className="text-sm text-gray-700">{subheading}</div>
   </div>
 );
 
 export default Achieve;
-
